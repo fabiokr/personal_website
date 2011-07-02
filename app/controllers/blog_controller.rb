d866fe121ac223e36b44dc5f4e986e268eb13c0e
Fabio Kreusch <fabiokr@gmail.com>
2011-07-02 16:50:05 -0700

    Setting locale from blog article

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -10,6 +10,8 @@ class BlogController < ApplicationController
     unless @article = Blog::Article.for_published_and_slug(Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i), params[:slug]).first
       raise ActiveRecord::RecordNotFound.new
     end
+
+    I18n.locale = @article.locale
   end
 
   private

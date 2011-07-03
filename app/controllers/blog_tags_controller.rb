90aa406d1fc176ea8ce9b8649dea2273c421e1a2
Fabio Kreusch <fabiokr@gmail.com>
2011-07-02 22:00:27 -0700

    Fixed tags

--- app/controllers/blog_tags_controller.rb
+++ app/controllers/blog_tags_controller.rb
@@ -6,6 +6,7 @@ class BlogTagsController < BlogController
 
   def show
     @articles = Blog::Article.for_locale(params[:locale]).published.sorted.page(params[:page]).tagged_with(params[:tag])
+    @tags = tags
     render 'blog/index'
   end
 
